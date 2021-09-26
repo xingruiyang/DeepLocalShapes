d93e2c17e75bb94dbc91a9fd12086c53a14ec925
Yang Xing Rui <64749983+xingruiy@users.noreply.github.com>
2021-09-26 13:19:01 +0000

    update

--- benchmark/eval.sh
+++ benchmark/eval.sh
@@ -2,8 +2,9 @@
 
 input=$1
 output=$2
-LATENT_SIZE=64
-CLAMP_DIST=-1
+network=$3
+LATENT_SIZE=$4
+CLAMP_DIST=$5
 VOXLE_SIZE=0.1
 NUM_EPOCHS=50
 
@@ -13,7 +14,7 @@ fi
 
 set -e
 
-if [ ! -z $3 ]; then
+if [ ! -z $6 ]; then
     normalized_output=$input;
     filename=$(echo $input|sed -e 's/\.[^./]*$//');
     normalized_input=$input
@@ -39,10 +40,10 @@ if [ ! -f $output/data/samples.pkl ]; then
         --transformer models/transformer.pth;
 fi
 
-if [ ! -f $output/aligned/ckpt_99_latents.npy ]; then 
+if [ ! -f $output/aligned/ckpt_49_latents.npy ]; then 
     echo "INFO: Optimizing aligned latent vectors";
     python3 local_shapes/optimize.py \
-        output/train/aligned/latest_model.pth \
+        $network/aligned/latest_model.pth \
         $output/data \
         $output/aligned \
         --batch_size 10000 \
@@ -54,10 +55,10 @@ if [ ! -f $output/aligned/ckpt_99_latents.npy ]; then
         --latent_size $LATENT_SIZE;
 fi
 
-if [ ! -f $output/unaligned/ckpt_99_latents.npy ]; then
+if [ ! -f $output/unaligned/ckpt_49_latents.npy ]; then
     echo "INFO: Optimizing unaligned latent vectors";
     python3 local_shapes/optimize.py \
-        output/train/unaligned/latest_model.pth \
+        $network/unaligned/latest_model.pth \
         $output/data \
         $output/unaligned \
         --batch_size 10000 \
@@ -67,56 +68,3 @@ if [ ! -f $output/unaligned/ckpt_99_latents.npy ]; then
         --gt_mesh $input \
         --latent_size $LATENT_SIZE;
 fi
-
-# for i in {0..99}
-# do 
-#     if [ ! -f $output/aligned/ckpt_${i}_mesh.ply ]; then
-#         echo "INFO: Reconstructing aligned shapes: ${i}";
-#         python3 local_shapes/reconstruct.py \
-#             $output/data \
-#             $output/aligned/ckpt_${i}_latents.npy \
-#             output/train/aligned/latest_model.pth \
-#             --orient \
-#             --interp \
-#             --latent_size $LATENT_SIZE \
-#             --output $output/aligned/ckpt_${i}_mesh.ply;
-#     fi
-# done
-
-# for i in {0..99}
-# do 
-#     if [ ! -f $output/unaligned/ckpt_${i}_mesh.ply ]; then
-#         echo "INFO: Reconstructing unaligned shapes: ${i}";
-#         python3 local_shapes/reconstruct.py \
-#             $output/data \
-#             $output/unaligned/ckpt_${i}_latents.npy \
-#             output/train/unaligned/latest_model.pth \
-#             --interp \
-#             --latent_size $LATENT_SIZE \
-#             --output $output/unaligned/ckpt_${i}_mesh.ply;
-#     fi
-# done
-
-# if [ ! -f $output/aligned/chamfer.txt ]; then
-#     for i in {0..99}
-#     do 
-#         echo "INFO: Calculating aligned chamfer distance: ${i}";
-#         python3 benchmark/cal_chamfer.py \
-#             $input \
-#             $output/aligned/ckpt_${i}_mesh.ply \
-#             --normalize \
-#             >> $output/aligned/chamfer.txt;
-#     done
-# fi
-
-# if [ ! -f $output/unaligned/chamfer.txt ]; then
-#     for i in {0..99}
-#     do 
-#         echo "INFO: Calculating unaligned chamfer distance: ${i}";
-#         python3 benchmark/cal_chamfer.py \
-#             $input \
-#             $output/unaligned/ckpt_${i}_mesh.ply \
-#             --normalize \
-#             >> $output/unaligned/chamfer.txt;
-#     done
-# fi
--- benchmark/run_tasks.sh
+++ benchmark/run_tasks.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
-./benchmark/train.sh examples/table.stl output/table_train 32 0.1 1
-./benchmark/train.sh examples/table.stl output/table_train2 32 0.3 1
-./benchmark/train.sh examples/table.stl output/table_train 16 0.1 1
+# Train the network on random shapes
+
+echo "Generating random shapes...";
+
+if [ ! -f examples/train.ply ]; then
+    python3 samplers/random_mesh.py \
+        --num_shapes 30 \
+        --export examples/train.ply;
+fi
+
+if [ ! -f examples/eval.ply ]; then
+    python3 samplers/random_mesh.py \
+        --num_shapes 30 \
+        --export examples/eval.ply;
+fi
+
+echo "Train on random shapes...";
+./benchmark/train.sh examples/train.ply output/125/train 125 0.1
+./benchmark/train.sh examples/train.ply output/64/train 64 0.1
+./benchmark/train.sh examples/train.ply output/32/train 32 0.1
+
+echo "Evaluate on random shapes...";
+./benchmark/eval.sh examples/eval.ply output/125/eval output/125/train 125 0.1
+./benchmark/eval.sh examples/eval.ply output/64/eval output/64/train 64 0.1
+./benchmark/eval.sh examples/eval.ply output/32/eval output/32/train 32 0.1
+
+
+echo "Evaluate on chairs...";
+./benchmark/eval.sh examples/chair.stl output/125/chair output/125/train 125 0.1 1
+./benchmark/eval.sh examples/chair.stl output/64/chair output/64/train 64 0.1 1
+./benchmark/eval.sh examples/chair.stl output/32/chair output/32/train 32 0.1 1
+
+echo "Evaluate on tables...";
+./benchmark/eval.sh examples/table.stl output/125/table output/125/train 125 0.1 1
+./benchmark/eval.sh examples/table.stl output/64/table output/64/train 64 0.1 1
+./benchmark/eval.sh examples/table.stl output/32/table output/32/train 32 0.1 1
